1204c5726a576616f8a37a6f4f6cb3da15d8c0f8
RamadhanDZR <165451756+RamadhanDZR@users.noreply.github.com>
2024-03-30 11:29:38 +0700

    pH

---
+++ test.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.header('Penentuan pH', divider='rainbow')
+
+angka_pertama = st.number_input('molaritas')
+st.write('The first number is ', angka_pertama)
+
+angka_kedua = st.number_input('valensi')
+st.write('ph', angka_kedua)
+
+operasi_matematika = angka_pertama * angka_kedua
+st.write(f"molaritas {angka_pertama} x ph {angka_kedua} = OH {operasi_matematika}")
